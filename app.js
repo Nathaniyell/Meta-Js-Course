d63511313f454be3df83824a73a82d883646a49e
Nathaniyell <essien.nathan@yahoo.com>
2023-09-03 23:08:22 +0100

    practice with variables

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 console.log("Hello World")
+var person;
+// "=" is called the assignment operator
+person = "John"
+console.log("Hello", person)
+
+var petDog = "Rex"
+var petCat = "Pepper"
+// console.log(petDog)
+// console.log(petCat)
+console.log("My Dog's name is :", petDog)
+console.log("My Dog's name is :", petCat)
+var catSound = "purr"
+var dogSound = "woof"
+console.log(petCat + " says " + catSound)
+catSound = "meow"
+console.log(petCat + " says " + catSound)
