c8446b2ea7d04c651aa490b787e9e962045b51cd
Nathaniyell <essien.nathan@yahoo.com>
2023-09-04 01:31:38 +0100

    add dataTypes and Operators

--- app.js
+++ app.js
@@ -15,3 +15,27 @@ var dogSound = "woof"
 console.log(petCat + " says " + catSound)
 catSound = "meow"
 console.log(petCat + " says " + catSound)
+
+//There are seven data types in javascript
+const dataTypes = ["string", "number", "null", "undefined", "Boolean", "Bigint", "symbol"]
+//Text Values are known as the string data type while numerical value are known as the number data type
+//Null represents the absence of a value
+//Undefined represents a variable that has not yet been assigned a value
+//BigInt is a data type that can accomodate a greater range of numbers that the number data type
+//A symbol data type is used as unique identifier
+
+const operators ={
+    assignment: "=",
+    multiplication: "*",
+    addition: "+",
+    subtraction: "-",
+    division: "/",
+    greaterThan: ">",
+    lessThan: "<",
+    equualTo: "==",
+    notEquualTo: "!=",
+    logicalOperators:{
+        "&&": "which checks and ensures that both conditions are true",
+        "||": "this is used to ensure that at least one of the conditions is true",
+    }
+}
