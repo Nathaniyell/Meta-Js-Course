1088db37d919590005b85d53845b01cd83e6d372
Nathaniyell <essien.nathan@yahoo.com>
2023-09-05 02:06:25 +0100

    Loops defined

---
+++ loops.js
@@ -0,0 +1,18 @@
+/*Loops are used to continually execute repeated blocks of code until certain conditions are satisfied
+Types of loops include: for, while and nested loops
+Properties of this loops includes 1. a counter variable - 
+var counter(i)= value
+2. a condition - counter > 5
+3. the counter must be incremented (increased or decreased depending on the need) - counter++ or counter--
+for(var i = 0; i<5; i++){
+    console.log(i)
+}
+
+The while loop is similar but the start counter is set outside the while loop and the incrementer is set inside the loop's body
+var i = 0
+while(i<5){
+    console.log(i)
+i++
+}
+
+*/
