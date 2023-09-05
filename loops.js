6fb5026367c17e273507c0caadecc554f8500bdd
Nathaniyell <essien.nathan@yahoo.com>
2023-09-05 03:24:10 +0100

    add example of switch-case and for loop

--- loops.js
+++ loops.js
@@ -22,32 +22,56 @@ for (var i = 10; i > 0; i--) {
 }
 console.log("Happy New Year!");
 
-var counter = 5
-while(counter > 0){
-    console.log(counter);
-    counter = counter - 1
+var counter = 5;
+while (counter > 0) {
+  console.log(counter);
+  counter = counter - 1;
 }
 var year = 2018;
 while (year < 2023) {
-    console.log(year);
-    year++;
-};
+  console.log(year);
+  year++;
+}
 
 //Nested loops are used to perform multiple tasks at the same time as shown below
 //It is important to note that the more nested loops there are, the more slow the code will run
 //can be used in instances where one needs automated repetition of code e.g sorting numbers from highest to lowest
 
-for(var week = 1; week<=2; week++){
-    console.log(week);
-    for(var day = 1; day<=5; day++){
-        console.log("Week " + week + " Day " + day)
-    }
+for (var week = 1; week <= 2; week++) {
+  console.log(week);
+  for (var day = 1; day <= 5; day++) {
+    console.log("Week " + week + " Day " + day);
+  }
 }
 
 //To know the summer months of each year from 2018 to 2023
-for (var year = 2018; year<2024; year++){
-    console.log(year)
-    for(var month = 6; month<9; month++){
-console.log("......... " + month)
+for (var year = 2018; year < 2024; year++) {
+  console.log(year);
+  for (var month = 6; month < 9; month++) {
+    console.log("......... " + month);
+  }
+}
+
+//Multiplying two numbers together
+
+for (var firstNum = 0; firstNum < 3; firstNum++) {
+  for (var secondNum = 0; secondNum < 10; secondNum++) {
+    console.log(firstNum + " times " + secondNum + " is equal to " + firstNum*secondNum);
+  }
+}
+for(var i = 1; i<=10; i++){
+    switch(i){
+        case 1:
+        console.log("Gold Medal");
+        break;
+        case 2:
+        console.log("Silver Medal");
+        break;
+        case 3:
+        console.log("Bronze Medal");
+        break;
+        default:
+        console.log(i);
+        break
     }
 }
