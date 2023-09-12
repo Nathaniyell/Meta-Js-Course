125bc5e3bfde11e890f05025d94bc520cc45ba49
Nathaniyell <essien.nathan@yahoo.com>
2023-09-12 21:09:05 +0100

    add parameters and argument to function

--- functions.js
+++ functions.js
@@ -1,6 +1,14 @@
 // Functions help in running the same set of stored instructions repeatedly without requiring you to adjust the settings every time
 
-function addNums(){ 
-    //body of function
+function addNums() {
+  //body of function
 }
-addNums() //invoke function
+addNums(); //invoke function
+
+//values passed to the function are known as function parameters and are placed inside the function definition
+
+function addTwoNums(a, b) {
+  var c = a + b;
+  console.log(c);
+}
+addTwoNums(10, 20); //the values passed to the function when invoking it are called arguments
