5be69b4987b197e4a7ec1d07d50b23211ea150f5
Nathaniyell <essien.nathan@yahoo.com>
2023-09-12 21:04:27 +0100

    add how to define and invoke a function

--- functions.js
+++ functions.js
@@ -1,1 +1,6 @@
 // Functions help in running the same set of stored instructions repeatedly without requiring you to adjust the settings every time
+
+function addNums(){ 
+    //body of function
+}
+addNums() //invoke function
