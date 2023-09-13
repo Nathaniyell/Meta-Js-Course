4405344fb3713df175c24b94876615e4e58b6172
Nathaniyell <essien.nathan@yahoo.com>
2023-09-13 06:14:43 +0100

    add objects and examples

---
+++ objects.js
@@ -0,0 +1,12 @@
+//Objects are used to store related data that describe the same variable
+//Object can be described as collection of related properties
+
+//each property is described as a key-value pair, where the usual variableName is the property key while the usual value is the property value here
+
+var storeManager={};
+storeManager.Movement =4;
+storeManager.SocialSkills = 50;
+storeManager.StreetSmart =50;
+storeManager.Health = 30;
+console.log(storeManager)
+//The above uses the dot notation to build a store manager object
