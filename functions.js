9d1dba7dc067893985a33031f52f1b14ceadc109
Nathaniyell <essien.nathan@yahoo.com>
2023-09-13 16:19:37 +0100

    add concept of Higher Order Functions and Pure Functions

--- functions.js
+++ functions.js
@@ -68,3 +68,31 @@ return mph * h
 var mph = 60
 var h = 4
 var distance = getDistance(mph, h)
+
+/* 
+A higher-order function is a function that has either one or both of the following characteristics:
+It accepts other functions as arguments
+It returns functions when invoked
+*/
+function addTwoNums(getNumber1, getNumber2) {
+  console.log(getNumber1() + getNumber2());
+}
+//the above function declaration of addTwoNums as describing how it will deal with the getNumber1 and getNumber2 inputs: once it receives them as arguments, it will then attempt invoking them and concatenating the values returned from those invocations.
+
+//A pure function returns the exact same result as long as it's given the same values.
+function addTwoNums(a, b) {
+  console.log(a + b)
+}
+//This function will always return the same output, based on the input. For example, as long as we give it a specific value, say, 5, and 6: 
+addTwoNums(5, 6); //returns 11
+
+/*
+Another rule for a function to be considered pure is that it should not have side-effects. A side-effect is any instance where a function makes a change outside of itself.
+
+This includes: 
+changing variable values outside of the function itself, or even relying on outside variables 
+calling a Browser API (even the console itself!) 
+calling Math.random() - since the value cannot be reliably repeated
+
+
+*/
