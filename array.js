00ca72fa2cbf940c2b5e06b931678541e0b69ab0
Nathaniyell <essien.nathan@yahoo.com>
2023-09-13 07:01:16 +0100

    add arithmetic object methods

--- array.js
+++ array.js
@@ -5,8 +5,20 @@ console.log(train1[0]); //can be used to access the value with the index of 0
 
 //arrays are objects because they have some built-in properties and methods e.g pop(),push()
 
-var fruits =[]
-fruits.push("apple")
-fruits.push("banana")
-console.log(fruits)
+var fruits = [];
+fruits.push("apple");
+fruits.push("banana");
+console.log(fruits);
 //The push() method adds a new item to the array while the pop() method removes the last item from an array
+
+function arrayBuilder(one, two, three) {
+  var arr = [];
+  arr.push(one);
+  arr.push(two);
+  arr.push(three);
+ return arr
+}
+//Additionally, the function call can be saved to a variable
+
+var simpleArr = arrayBuilder('apple', 'pear', 'plum')
+console.log(simpleArr);
