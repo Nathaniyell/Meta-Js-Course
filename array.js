18fb1f4c49ac043530da3509aabc0aef9beccef1
Nathaniyell <essien.nathan@yahoo.com>
2023-09-13 06:05:58 +0100

    add array example, add letterfinder function

--- array.js
+++ array.js
@@ -1,3 +1,4 @@
 //arrays are used to group a sequence of variables in a collection
 
-var train1 = ["wheat", "barley", "patato", "salt", "rocks"]
+var train1 = ["wheat", "barley", "patato", "salt", "rocks"];
+console.log(train1[0]); //can be used to access the value with the index of 0
--- functions.js
+++ functions.js
@@ -15,3 +15,15 @@ function addTwoNums(a, b) {
 addTwoNums(10, 20); //the values passed to the function when invoking it are called arguments
 
 //Function parameters are like variables and the actual values of these parameters are called arguments and are made available when the function is invoked
+
+//LetterFinder function
+function letterFinder(word, match){
+  for(var i = 0; i < word.length; i++){
+            if(word[i]==match){
+          console.log("found the", match, "at", i )
+      }else{
+          console.log('---No match found at', i)
+      }
+  }
+}
+letterFinder("test", "t")
