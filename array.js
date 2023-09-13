fd7bd641ceb0107ffb6bd73953ebbfe9b3fce6ca
Nathaniyell <essien.nathan@yahoo.com>
2023-09-13 06:50:00 +0100

    add array methods

--- array.js
+++ array.js
@@ -2,3 +2,11 @@
 
 var train1 = ["wheat", "barley", "patato", "salt", "rocks"];
 console.log(train1[0]); //can be used to access the value with the index of 0
+
+//arrays are objects because they have some built-in properties and methods e.g pop(),push()
+
+var fruits =[]
+fruits.push("apple")
+fruits.push("banana")
+console.log(fruits)
+//The push() method adds a new item to the array while the pop() method removes the last item from an array
