0bbcb9b5c88e32988f8ef3a4fad348737ed7dece
Nathaniyell <essien.nathan@yahoo.com>
2023-09-21 20:31:35 +0100

    add object.keys method for better understanding of the for-of-loop

--- forOfLoop.js
+++ forOfLoop.js
@@ -15,3 +15,10 @@ for (var color of colors) {
     console.log(color);
 }
 //The output of the above code is as follows: red, oranage, yellow
+
+//The for of loop can be run on arrays and also can be used to loop over objects. To do this, you need to use the Object.keys() method, which receives an object as its parameter. Remember, this object is the object you want to loop over. An array of properties is returned when you call the Object.keys() method as shown:
+const car2 = {
+    speed: 200,
+    color: "red"
+}
+console.log(Object.keys(car2)); // ['speed','color']
