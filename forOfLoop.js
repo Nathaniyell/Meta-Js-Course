5a25dd86e5889351671f604cb52b794bea539ecb
Nathaniyell <essien.nathan@yahoo.com>
2023-09-22 03:00:16 +0100

    add array-and-object-iteration assignment

--- forOfLoop.js
+++ forOfLoop.js
@@ -74,3 +74,47 @@ made possible
 with bac-ticks in
 ES6
 `
+console.log(multiLineString);
+
+// Task 1
+var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake']
+
+function logDairy(){
+    for(var item of dairy){
+        console.log(item)
+    }
+}
+
+// Task 2
+const animal = {
+
+    canJump: true
+
+};
+
+const bird = Object.create(animal);
+
+bird.canFly = true;
+
+bird.hasFeathers = true;
+
+function birdCan() {
+    for (var prop of Object.keys(bird)) {
+        console.log(prop + ": " +  bird[prop])
+    }
+}
+
+// Task 3
+function animalCan() {
+    for (var prop of Object.keys(bird)) {
+        console.log(prop + ": " +  bird[prop])
+    }
+    for (var prop of Object.keys(animal)) {
+        console.log(prop + ": " +  animal[prop])
+    }
+}
+
+
+logDairy();
+birdCan();
+animalCan();
