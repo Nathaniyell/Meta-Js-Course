35e6ff8d9cf84e24917acbd149bb0e0a228da367
Nathaniyell <essien.nathan@yahoo.com>
2023-09-22 03:08:52 +0100

    add concept of data structures with javascript

---
+++ dataStructures,.js
@@ -0,0 +1,7 @@
+//A data structure is a way to organize data. Examples of common javascript data structures include: objects, arrays, maps and sets
+//An object is unaltered, noniterable collection of key value pairs and you use objects when you need to store and later access a value under a key
+
+//An array which is an ordered iterable collection of values. Likewise, you use arrays when you need to store and later access a value under an index
+
+const grades = [75, 95, 90, 85, 80, 85]
+grades.map(grade=>grade)
