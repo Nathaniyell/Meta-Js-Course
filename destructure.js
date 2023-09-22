bdc710536a895969edcb617ea5e42e6c64466dd5
Nathaniyell <essien.nathan@yahoo.com>
2023-09-22 04:11:30 +0100

    add concept of spread operators

--- destructure.js
+++ destructure.js
@@ -9,3 +9,10 @@ const { a, b } = obj;
 // is equivalent to:
 // const a = obj.a;
 // const b = obj.b;
+
+//The spread operator is an addition that came with ES6. It is the shortest and simplest method to copy the properties of an object onto a newly created object. It is a multipurpose tool that can spread out array items and join objects together, it can be used to unpack an array
+
+//The spread operator is characterized by three dots '...'
+let top4EplClubs = ['Mancity', "Arsenal", "Liverpool", "Newastle"];
+let top6EplClubs = [...top4EplClubs, "Man-united", "Chelshit"]
+console.log(top6EplClubs)
