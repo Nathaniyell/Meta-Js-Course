92c8849875870f19a82ff44dd4bf8d26f79f7780
Nathaniyell <essien.nathan@yahoo.com>
2023-09-24 22:13:08 +0100

    add definition and examples of e2e testing

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+//Testing is how you verify the expectations you have regarding the behaviour of your code
 function concatStrings(strA, strB) {
   return strA + strB;
 }
@@ -10,3 +11,15 @@ expect(concatStrings("abc", "def")).toBe("abcdef") //this is essentially stating
 //Testing helps you verify that the function is behaving in the way you intended.
 //Testing code ensures: conciseness,clarity and repeatability
 //You can run these tests each time you want to instantiate a function
+//Changing code structure without changing its functionality is known as refactoring
+
+//Types of testing
+//e2e - End to End testing: tries to imitate how a usier might interact with your app. Here you need to open your web application in a browser and then test it by interacting with it how a user might interact with it. In other words you are testing the entired finished software product from the perspective of the user. e2e tests are slow and time consuming
+//Examples of e2e testing frameworks: WebdriverJs, Protractor and Cypress
+
+
+
+//Integration testing
+
+
+//Unit testing
