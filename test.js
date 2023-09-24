acc68236b256ac9abf982af8f89fb1f048b6b4af
Nathaniyell <essien.nathan@yahoo.com>
2023-09-24 21:46:05 +0100

    add details about how testing works in JS

---
+++ test.js
@@ -0,0 +1,6 @@
+function concatStrings(strA, strB) {
+  return strA + strB;
+}
+concatStrings("abc", "def") // returns "abcdef"
+
+concatStrings(1,2) // returns 12 which is the wrong response for the function since it was numbers that was passed in
