fb90442c3e157ca49694f0647ae5078499c9a22c
Nathaniyell <essien.nathan@yahoo.com>
2023-09-24 21:42:15 +0100

    add details of npm

--- NodeDetails.js
+++ NodeDetails.js
@@ -1,1 +1,5 @@
 //Node.js is a separate standalone environment. The existence of Node makes it possible to use javascript on the backend/server-side, making it possible to build a fullstack Javascript application
+
+//Node.js comes with a packet manager (NPM), which allows you to use large number of libraries and frameworks as node.js modules
+//An NPM module is a standalone piece of code that has been published on the npm website, which is sometimes referred to as an npm package
+//the npm command is used to install any node moule from the npm repository
