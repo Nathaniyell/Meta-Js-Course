04f6791bd324c11085f534b14aeb6e52e5963dcf
Nathaniyell <essien.nathan@yahoo.com>
2023-09-24 23:02:58 +0100

    add concept of mocking in JEST

--- test.js
+++ test.js
@@ -28,3 +28,8 @@ expect(concatStrings("abc", "def")).toBe("abcdef") //this is essentially stating
 //Jest is a javascript testing framework which is often used for testing code like React. It is maintained by META, it can also allow yoy to test Babel, TypeScript,Node,Angulat and various other frameworks
 
 //Code coverage is a measure of what percentage of your code is covered by tests. Code coverage is a handy tool to guage the amount of your codebase that is included tests. The higher the code coverage, the lower to the chance to have unidentified bugs
+
+//Mocking allows you to separate the code that you are testing from its related dependencies i.e you can use mocking features to ensure that your unit testing is stand-alone.
+//Mocks, allow you to pretend that the users are already there. The needed data comes from the mock rather than from the backend. This allows the front-end developers to finish their site of the new feature independently. In certain cases, developers can use mocking to ship features faster. The great thing about Jest is that you use it's mock functions without any additional installations. In Jest you use mocking by employing Jest mock functions. It's also easy to test asynchronous code in Jest.
+
+//Jest allows you to perform snapshot testing - Snapshot testing is used to verify that there are no regressions in the DOM of your apps
