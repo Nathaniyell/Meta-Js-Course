19fdb83e76778b5cda21737a0fa0c5837a790b2a
Nathaniyell <essien.nathan@yahoo.com>
2023-09-25 22:58:21 +0100

    add description of capstone project

---
+++ littlePokemon.js
@@ -0,0 +1,8 @@
+/*
+Capstone Project
+In this exercise, you will work with some data provided as an array of objects, listing information about dishes available in the Little Lemon restaurant.
+You will need to write a function declaration which will be able to do two things:
+If the function is called with the argument true, it will output the names of the dishes and calculate their final price (including 20% tax)
+
+*/
+
