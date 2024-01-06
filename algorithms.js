b838625cf779a99ed7ff99bcf0d326020ed9237f
Nathaniyell <essien.nathan@yahoo.com>
2024-01-06 05:42:17 +0100

    update algorithms.js

--- algorithms.js
+++ algorithms.js
@@ -131,3 +131,4 @@ function getNthPermutation(n, digits) {
   /*
   The package.json file lists information pertaining to my app, which allows npm to run several scripts and perform various tasks in the app itself. The package-loc.json file holds the list of all dependencies with a specific versions. The package.json file helps npm rebuild the app on another machine. Or if we delete the node modules folder with all the files that our project needs to run, the package-loc.json file has all the information for npm to be able to rebuild those files reliably. This file is there to ensure the npm tracks all the modules installations properly. As a general rule, it's better to leave these files where they are at the moment as they are required for the app to function.
   */
+ //If the links are internal to the app - using the <a> tag is simply not the React way of doing things. 
